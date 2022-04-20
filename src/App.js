9d216c32c0f99ae071f9df4877e21b964eeb3a8b
Rodriques <rodriques.perry@gmail.com>
2022-04-19 20:25:48 -0500

    update: Class component, componentDidMount() lifecycle method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+    console.log(1);
+  }
+
+/* 
+Lifecycle method #1 
+ComponentDidmount: This is the moment your information gets placed on the DOM
+Any time you have a "Class" component and you need to leverage an API call do so inside this method
+*/
+  componentDidMount() {
+    console.log(3);
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    console.log(2);
+    return (
+      <div className="App">
+        {/*  This shows doing it manually compared 
+        to doing it with the map() method below 
+        
+        // Remember map() returns a new array 
+        
+        <h1>{this.state.monsters[0].name}</h1>
+        <h1>{this.state.monsters[1].name}</h1>
+        <h1>{this.state.monsters[2].name}</h1>
+        */}
+
+        {/* Remember to place the "key" 
+        attribute in the highest possible element on the return */}
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
